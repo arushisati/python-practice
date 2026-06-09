da039ba9e910c5dd3e17b5d0218562e6de7a1f45
ARUSHI SATI <arushisati25@gmail.com>
2026-06-09 14:26:40 +0530

    Create if_elif_grade.py

---
+++ if_elif_grade.py
@@ -0,0 +1,10 @@
+marks= 99
+if(marks>=90):
+    print("grade a")
+elif(marks>=80):
+    print("grade b and marks<90")
+elif(marks>=70):
+    print("grade c and marks<80")
+else:
+    print("fail")
+        
