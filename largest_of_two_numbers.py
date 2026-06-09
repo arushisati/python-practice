83c35b803c41e75959e8607904ce892e63f9117c
ARUSHI SATI <arushisati25@gmail.com>
2026-06-09 14:39:08 +0530

    Create largest_of_two_numbers.py

---
+++ largest_of_two_numbers.py
@@ -0,0 +1,9 @@
+number1= int(input("Enter a number:"))
+number2= int(input("Enter a number:"))
+if(number1>number2):
+    print("number 1 is greater")
+elif(number1<number2):
+    print("number2 is greater")
+else:
+    print("both are equal")
+        
