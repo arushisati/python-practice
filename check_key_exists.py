d064f408c3c7586901c11dfad7f9ad4bc9842a95
ARUSHI SATI <arushisati25@gmail.com>
2026-06-13 21:53:54 +0530

    Create check_key_exists.py

---
+++ check_key_exists.py
@@ -0,0 +1,11 @@
+student = {
+    "name": str(input("ENTER A NAME")),
+    "age": int(input("ENTER A NUMBER")),
+    
+}
+if"age" in student:
+    print("key exist")
+else:
+    print("key do not exist")
+
+
