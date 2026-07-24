7f334e37270d34fa4a612be1114798d3b90c2371
ARUSHI SATI <arushisati25@gmail.com>
2026-07-24 18:57:17 +0530

    Create numpy_slicing.py

---
+++ numpy_slicing.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+arr = np.array([10,20,30,40,50])
+
+print(arr[:3])    # First 3
+print(arr[2:])    # From index 2
+print(arr[1:4])   # Index 1 to 3
+print(arr[::2])   # Every 2nd
+print(arr[::-1])  # Reverse
